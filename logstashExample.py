7aa16c0330590ba80e8897ae7de160e4f4e72779
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-11-21 16:23:28 +0530

    successful logstash, but unable to give extras

--- logstashExample.py
+++ logstashExample.py
@@ -1,18 +1,32 @@
 import logging
 import sys
 from logstash_async.handler import AsynchronousLogstashHandler
+from logstash_async.formatter import LogstashFormatter
 from config import getConfig
 import json
 
+# logging configuration
 appConfig = getConfig()
 host: str = appConfig["logstash_host"]
 port: int = appConfig["logstash_port"]
 
+# formatting for log stash
+logstash_formatter = LogstashFormatter(
+    message_type='python-logstash',
+    extra_prefix='dev',
+    extra=dict(application='example-app', environment='production'))
+
 test_logger = logging.getLogger('python-logstash-logger')
 test_logger.setLevel(logging.INFO)
-# test_logger.addHandler(AsynchronousLogstashHandler(
-#     host, port, database_path='logstash.db'))
-test_logger.addHandler(logging.StreamHandler())
+
+streamHandler = logging.StreamHandler()
+# streamHandler.setFormatter(logstash_formatter)
+test_logger.addHandler(streamHandler)
+
+logstashHandler = AsynchronousLogstashHandler(
+    host, port, database_path='logstash.db')
+logstashHandler.setFormatter(logstash_formatter)
+test_logger.addHandler(logstashHandler)
 
 # If you don't want to write to a SQLite database, then you do
 # not have to specify a database_path.
@@ -25,11 +39,17 @@ test_logger.warning('python-logstash-async: test logstash warning message.')
 
 # add extra field to logstash message
 extra = {
-    'test_string': 'python version: ' + repr(sys.version_info),
-    'test_boolean': True,
-    'test_dict': {'a': 1, 'b': 'c'},
-    'test_float': 1.23,
-    'test_integer': 123,
-    'test_list': [1, 2, '3'],
+    "test_string": "python_version-" + repr(sys.version_info),
+    "test_boolean": True,
+    "test_dict": {"a": 1, "b": "c"},
+    "test_float": 1.23,
+    "test_integer": 123,
+    "test_list": [1, 2, "3"],
 }
 test_logger.info('python-logstash: test extra fields', extra=extra)
+
+try:
+    x = 1 / 0
+except Exception as e:
+    test_logger.error("Some error occured", exc_info=e)
+    x = 1
