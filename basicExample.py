4e752ccafabd3541d2431e90be36f95ca96f15a7
nagasudhirpulla <nagasudhirpulla@gmail.com>
2020-11-21 11:14:29 +0530

    first commit

---
+++ basicExample.py
@@ -0,0 +1,36 @@
+import logging
+import json
+from logging import LoggerAdapter
+
+
+def getEnrichedLogger(name: str, extra: dict) -> LoggerAdapter:
+    """get logger object that is enriched with the 'extra' dict
+    https://medium.com/devops-dudes/python-logs-a-jsons-journey-to-elasticsearch-ffbabfd44b83
+
+    Args:
+        name (str): name of logger
+        extra (dict): enrich dict, like {"app_name":"myApp", "server_ip":"10.10.10.10"}
+
+    Returns:
+        LoggerAdapter: LoggerAdapter object
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    streamHandler = logging.StreamHandler()
+    basicDict = {
+        "time": "%(asctime)s", "level": "%(levelname)s", "message": "%(message)s"}
+    fullDict = {**basicDict, **extra}
+    streamFormatter = logging.Formatter(json.dumps(fullDict))
+    streamHandler.setFormatter(streamFormatter)
+    logger.addHandler(streamHandler)
+    logger = logging.LoggerAdapter(logger, extra)
+    return logger
+
+
+logger = getEnrichedLogger(name="test_app", extra={"app_name": "myTestApp"})
+logger.info("Hello World!!!")
+try:
+    x = 1/0
+except Exception as e:
+    logger.error("Some error occured", exc_info=e)
+    # logger.exception("Some error occured")
---
+++ fileHandlerExample.py
@@ -0,0 +1,46 @@
+import logging
+from logging.handlers import RotatingFileHandler
+from logging import LoggerAdapter
+import json
+
+
+def getEnrichedLogger(name: str, fPath: str, maxBytes: int, backupCount: int, extra: dict) -> LoggerAdapter:
+    """get logger object that is enriched with the 'extra' dict
+    https://medium.com/devops-dudes/python-logs-a-jsons-journey-to-elasticsearch-ffbabfd44b83
+
+    Args:
+        name (str): name of logger
+        extra (dict): enrich dict, like {"app_name":"myApp", "server_ip":"10.10.10.10"}
+        maxBytes (int): max size of log file
+        backupCount (int): number of files to be retained
+
+    Returns:
+        LoggerAdapter: LoggerAdapter object
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+    streamHandler = logging.StreamHandler()
+    fileHandler = RotatingFileHandler(
+        fPath, maxBytes=maxBytes, backupCount=100)
+    fileHandler.namer = lambda name: name.replace(".log", "") + ".log"
+    basicDict = {
+        "time": "%(asctime)s", "level": "%(levelname)s", "message": "%(message)s"}
+    fullDict = {**basicDict, **extra}
+    streamFormatter = logging.Formatter(json.dumps(fullDict))
+    streamHandler.setFormatter(streamFormatter)
+    fileHandler.setFormatter(streamFormatter)
+    logger.addHandler(streamHandler)
+    logger.addHandler(fileHandler)
+    logger = logging.LoggerAdapter(logger, extra)
+    return logger
+
+
+logger = getEnrichedLogger(
+    "test", 'log/test.log', 50, 100, {"app_name": "myTestApp"})
+
+logger.info("Hello World!!!")
+try:
+    x = 1/0
+except Exception as e:
+    logger.error("Some error occured", exc_info=e)
+    # logger.exception("Some error occured")
